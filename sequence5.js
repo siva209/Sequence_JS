fe24772245d66850520f28a39429f1a1e5fe52b0
siva209 <sivanarayana209@gmail.com>
2021-06-29 21:44:52 +0530

    Added:Unit Conversion Program

---
+++ sequence5.js
@@ -0,0 +1,8 @@
+//Unit conversion
+let foot = 12;
+let inch = 0;
+console.log("42 inch : " + (42 / foot));
+let area = 2400 * 0.3048;
+console.log("Rectangular plot of 60 * 40 ft : " + area);
+let totalArea = (area * 25) / 4046.86;
+console.log("Area of 25 such plots: " + totalArea);
