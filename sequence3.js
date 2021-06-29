eefa022db7c24e24eb44d463a890bbc9f769754a
siva209 <sivanarayana209@gmail.com>
2021-06-29 21:41:04 +0530

    Added:Program to find sum of the numbers on dice when rolled

---
+++ sequence3.js
@@ -0,0 +1,6 @@
+//Random function to print two dice numbers
+let dice1 = Math.floor((Math.random() * 6) + 1);
+console.log(" Number 1 is " + dice1);
+let dice2 = Math.floor((Math.random() * 6) + 1);
+console.log(" Number 2 is " + dice2);
+console.log("Sum is : " + (dice1 + dice2));
